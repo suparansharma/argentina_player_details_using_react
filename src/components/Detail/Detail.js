87cf3c12b482d3f9f109e7dea799db4880fa85a2
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-19 01:32:30 +0600

    all complete

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const Detail = () => {
+const Detail = (props) => {
+    const detail = props.detail;
+
+
+    let total =0;
+    for (let i = 0; i < detail.length; i++) {
+        const player = detail[i];
+        total = Math.round(total + player.salary);
+        parseInt(total);
+        
+    }
     return (
         <div>
-            
+            <h3 >All player</h3>
+            <h4>Player Select:{detail.length} </h4>
+            <p>Salary:{total}</p>
+            {/* <p><small>PRODUCT PRICE:{total}</small></p>
+            <p><small>SHIPPING COST:{shipping}</small></p> */}
         </div>
     );
 };
